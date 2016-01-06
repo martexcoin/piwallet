d6424afcb4dc54fca1d367cc52d41c50f21b2bd3
Levi Nobre <email@levinobre.com>
2016-01-05 21:16:15 -0300

    Atualizando dados técnicos
    Dados como o endereço de doação e link do block explorer foram
    atualizados

--- common.php
+++ common.php
@@ -1,23 +1,23 @@
 <?php
 session_start();
 
-define("WITHDRAWALS_ENABLED", true); //Disable withdrawals during maintenance
+define("WITHDRAWALS_ENABLED", true);
 
 include('jsonRPCClient.php');
 include('classes/Client.php');
 include('classes/User.php');
 
-// function by zelles to modify the number to bitcoin format ex. 0.00120000
+
 function satoshitize($satoshitize) {
    return sprintf("%.8f", $satoshitize);
 }
 
-// function by zelles to trim trailing zeroes and decimal if need
+
 function satoshitrim($satoshitrim) {
    return rtrim(rtrim($satoshitrim, "0"), ".");
 }
 
-$server_url = "/";  // website url
+$server_url = "/";
 
 $db_host = "localhost";
 $db_user = "root";
@@ -26,13 +26,13 @@ $db_name = "piwallet";
 
 $rpc_host = "localhost";
 $rpc_port = "8332";
-$rpc_user = "bitcoinrpc";
-$rpc_pass = "Cp68nBkCAADKkskaKSskaDKdmSYLtLJ";
+$rpc_user = "martexcoinrpc";
+$rpc_pass = "Cp58nBkCAADKksktKSskaDKdmSQLtPJ";
 
-$fullname = "Martexcoin"; //Website Title (Do Not include 'wallet')
-$short = "MXT"; //Coin Short (BTC)
-$blockchain_url = "http://blockchain.info/tx/"; //Blockchain Url
-$support = "suporte@martexcoin.org"; //Your support eMail
-$hide_ids = array(1); //Hide account from admin dashboard
-$donation_address = "13jy6rHB7HMgQBoYxQQXSM7TFTZZ6CDAAZ"; //Donation Address
+$fullname = "Martexcoin";
+$short = "MXT";
+$blockchain_url = "http://be.martexcoin.org:2750/tx/";
+$support = "suporte@martexcoin.org";
+$hide_ids = array(1);
+$donation_address = "M8DSVG13j3qpNDRbuuUBh5juQmSd15wLXH";
 ?>
