2a3c4ebd89dacf04128a68471ceeae0df1deb3b3
redconan <redconanoficial@gmail.com>
2016-03-30 18:44:26 -0300

    atualização da home

--- index.php
+++ index.php
@@ -351,9 +351,9 @@ break;
             break;
       }
    }
-   include("view/header.php");
+   //include("view/header.php");
    include("view/home.php");
-   include("view/footer.php");
+   //include("view/footer.php");
 }
 $mysqli->close();
 ?>
