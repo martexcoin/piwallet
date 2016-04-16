e79a847d2318d04a959e4166ca02e1a5303678ca
redconan <redconanoficial@gmail.com>
2016-04-16 01:01:58 -0300

    Update api.php

--- api.php
+++ api.php
@@ -3,11 +3,12 @@ define("IN_WALLET", true);
 include('common.php');
 $con=mysqli_connect("$db_host","$db_user","$db_pass","$db_name");
 $key = $_GET['key'];
+$hello = mt_rand();
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL. Make sure to edit the common.php file: " . mysqli_connect_error();
   }
-$result = mysqli_query($con,"SELECT * FROM users where secret = '$key' and authused=1");
+$result = mysqli_query($con,"SELECT * FROM users where api_key = '$key'");
 while($row = mysqli_fetch_array($result))
   {
 $id = $row['id'];
@@ -17,8 +18,9 @@ $twofactoren = $row['authused'];
 $isadmin = $row['admin'];
 $client = new Client($rpc_host, $rpc_port, $rpc_user, $rpc_pass);
 $apibal = $client->getBalance($username);
-$addr = $client->getAddress($username);;
+$addr = $client->getAddress($username);
 }
+
 if ($isadmin == 1)
  {
  $ifadmin = "true"; }
@@ -30,5 +32,13 @@ $iftwofactor = "true"; }
 else {
 $iftwofactor = "false";}
 echo json_encode(array("id" => "$id", "username" => "$username", "balance" => "$apibal", "addresses" => "$addr", "Support Pin" => "$pin", "Two Factor Enabled"=> "$iftwofactor", "admin" => "$ifadmin"));
+
+$newkey = sha1(md5("$id.$username.$hello"));
+$sql = "UPDATE users SET api_key='$newkey' where api_key = '$key'";
+if ($con->query($sql) === TRUE) {
+	echo "Key Trocada Automaticamente";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
 ?>
 
